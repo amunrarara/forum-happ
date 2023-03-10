4dc6528ca09206931f4eda44892982841d3cc55a
Acea Spades <aceaspades@protonmail.com>
2023-03-10 20:35:46 +0200

    Exercise 2 Step 2 complete

--- dna/zomes/comments/src/lib.rs
+++ dna/zomes/comments/src/lib.rs
@@ -4,3 +4,28 @@ use hdk::prelude::*;
  * Add your edits to the bottom of this file
  */
 pub use comments_zome;
+
+#[hdk_entry_helper]
+struct Comment {
+    comment: String,
+}
+
+
+#[hdk_entry_defs]
+#[unit_enum(UnitTypes)]
+enum EntryTypes {
+    Comment(Comment)
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+struct CreateCommentInput {
+    comment_on: ActionHash,
+    comment: String
+}
+
+#[hdk_extern]
+fn create_comment(input: CreateCommentInput) -> ExternResult<ActionHash> {
+    let comment = Comment {comment: input.comment};
+    let ac_hash = create_entry(EntryTypes::Comment(comment))?;
+    Ok(ac_hash)
+}
